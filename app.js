72818e1cd344d89d1cb964b2cd8b944dd5f271fe
Daniel Ferreira <danthuggin@gmail.com>
2021-07-07 16:55:01 -0300

    Refatoração no código de adicionar horário.

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
-const addSchedule = document.querySelector('#addSchedule');
-const saveSchedule = document.querySelector('#saveSchedule');
-const resetSchedule = document.querySelector('#resetSchedule');
+const addScheduleButton = document.querySelector('#addScheduleButton');
+const saveScheduleButton = document.querySelector('#saveScheduleButton');
+const resetScheduleButton = document.querySelector('#resetScheduleButton');
 const containerAlternative = document.querySelector('.container-alternative');
-const printSchedule = document.querySelector('#printSchedule');
-const copySchedule = document.querySelector('#copySchedule');
-const hideSchedule = document.querySelector('#hideSchedule');
+const printScheduleButton = document.querySelector('#printScheduleButton');
+const copyScheduleButton = document.querySelector('#copyScheduleButton');
+const hideScheduleButton = document.querySelector('#hideScheduleButton');
+let lastIndex = 3;
 
 const addRowInTableMain = () =>
     `<tr class="timelines">
-        <th scope="row" data-main-index=""></th>
-        <td><input type="time" data-time="${++getScheduleValue().length}"></td>
-        <td><input type="checkbox" data-day="${++getScheduleValue().length}"></td>
-        <td><input type="checkbox" data-day="${++getScheduleValue().length}"></td>
-        <td><input type="checkbox" data-day="${++getScheduleValue().length}"></td>
-        <td><input type="checkbox" data-day="${++getScheduleValue().length}"></td>
-        <td><input type="checkbox" data-day="${++getScheduleValue().length}"></td>
-        <td><input type="checkbox" data-day="${++getScheduleValue().length}"></td>
-        <td><input type="checkbox" data-day="${++getScheduleValue().length}"></td>
+        <th scope="row" data-main-index="${lastIndex}">${lastIndex}</th>
+        <td><input type="time" data-time="${lastIndex}"></td>
+        <td><input type="checkbox" data-day="${lastIndex}"></td>
+        <td><input type="checkbox" data-day="${lastIndex}"></td>
+        <td><input type="checkbox" data-day="${lastIndex}"></td>
+        <td><input type="checkbox" data-day="${lastIndex}"></td>
+        <td><input type="checkbox" data-day="${lastIndex}"></td>
+        <td><input type="checkbox" data-day="${lastIndex}"></td>
+        <td><input type="checkbox" data-day="${lastIndex}"></td>
     </tr>`;
 
 const addRowInTableAlternative = () =>
     `<tr>
-        <th scope="row" data-alternative-index=""></th>
-        <td data-set-day="${getScheduleValue().length}"></td>
-        <td data-set-day="${getScheduleValue().length}"></td>
-        <td data-set-day="${getScheduleValue().length}"></td>
-        <td data-set-day="${getScheduleValue().length}"></td>
-        <td data-set-day="${getScheduleValue().length}"></td>
-        <td data-set-day="${getScheduleValue().length}"></td>
-        <td data-set-day="${getScheduleValue().length}"></td>
+        <th scope="row" data-alternative-index="${lastIndex}">${lastIndex}</th>
+        <td data-set-day="${lastIndex}"></td>
+        <td data-set-day="${lastIndex}"></td>
+        <td data-set-day="${lastIndex}"></td>
+        <td data-set-day="${lastIndex}"></td>
+        <td data-set-day="${lastIndex}"></td>
+        <td data-set-day="${lastIndex}"></td>
+        <td data-set-day="${lastIndex}"></td>
     </tr>`;
 
-//gera e insere automaticamente uma numeração para cada horário
-const setScheduleIndex = () => {
-    const dataMainIndex = document.querySelectorAll('[data-main-index]');
-    const dataAlternativeIndex = document.querySelectorAll('[data-alternative-index]');
-    dataMainIndex.forEach((element, index) => element.textContent = index + 1);
-    dataAlternativeIndex.forEach((element, index) => element.textContent = index + 1);
+//Adiciona uma nova linha de horário em ambas as tabelas
+const addScheduleRow = () => {
+    const tableMain = document.querySelector('.table-main');
+    const tableAlternative = document.querySelector('.table-alternative');
+
+    ++lastIndex;
+
+    tableMain.lastElementChild.insertAdjacentHTML('beforeend', addRowInTableMain());
+    tableAlternative.lastElementChild.insertAdjacentHTML('beforeend', addRowInTableAlternative());
 }
 
 //retorna um array com os valores dos inputs de horários
@@ -46,53 +50,47 @@ const getScheduleValue = () => {
 }
 
 //retorna um array contendo os 7 dias preenchidos com o horário informado ou a string "X"
-const getCheckboxValue = scheduleLine => {
-    const checkboxes = document.querySelectorAll(`[data-day="${scheduleLine}"]`);
+const getCheckboxValue = scheduleRow => {
+    const checkboxes = document.querySelectorAll(`[data-day="${scheduleRow}"]`);
+    
     return Array.from(checkboxes)
         .map(checkbox => checkbox.checked
-        ? document.querySelector(`[data-time="${scheduleLine}"]`).value: 'X');
+        ? document.querySelector(`[data-time="${scheduleRow}"]`).value: 'X');
 }
 
 //insere na tabela alternativa o array com os valores da tabela principal
 const setScheduleValue = scheduleIndex => {
     const dataSetDay = document.querySelectorAll(`[data-set-day="${scheduleIndex}"]`);
 
-    return Array.from(dataSetDay).forEach((setDay, index) => {
+    Array.from(dataSetDay).forEach((setDay, index) => {
         setDay.textContent = getCheckboxValue(scheduleIndex)[index];
     });
 }
 
-setScheduleIndex();
+
 
 //LISTENERS
-addSchedule.addEventListener('click', () => {
-    const tableMain = document.querySelector('.table-main');
-    const tableAlternative = document.querySelector('.table-alternative');
+addScheduleButton.addEventListener('click', addScheduleRow);
 
-    tableMain.lastElementChild.insertAdjacentHTML('beforeend', addRowInTableMain());
-    tableAlternative.lastElementChild.insertAdjacentHTML('beforeend', addRowInTableAlternative());
-    setScheduleIndex();
-});
-
-saveSchedule.addEventListener('click', () => {
+saveScheduleButton.addEventListener('click', () => {
     containerAlternative.classList.remove('hide');
     getScheduleValue().forEach((_, index) => setScheduleValue(index + 1))
 });
 
-resetSchedule.addEventListener('click', () => {
+resetScheduleButton.addEventListener('click', () => {
     document.location.reload();
 });
 
-printSchedule.addEventListener('click', () => {
+printScheduleButton.addEventListener('click', () => {
     print();
 });
 
-copySchedule.addEventListener('click', () => {
+copyScheduleButton.addEventListener('click', () => {
     const tableAlternative = document.querySelector('.table-alternative');
     navigator.clipboard.writeText(tableAlternative.innerText)
 });
 
-hideSchedule.addEventListener('click', event => {
+hideScheduleButton.addEventListener('click', event => {
     event.target.children[0].classList.toggle('fa-plus');
     containerAlternative.classList.add('hide');
 });
